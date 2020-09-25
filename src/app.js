b6e38bf77c6ef6fd4fdd8f3c63492604e3732c41
elefher <eldelielev@gmail.com>
2020-09-25 21:26:44 +0100

    install and setup facebook authentication

--- src/app.js
+++ src/app.js
@@ -2,27 +2,29 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose');
+const {getConnection} = require('./db')
+const {facebookInit} = require('./authentication')
 require('dotenv').config()
 
-const mongoDB = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-
-const db = mongoose.connection;
+const db = getConnection()
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+facebookInit(app)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
 module.exports = app;
---
+++ src/routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+router.get("/facebook", passport.authenticate("facebook", {scope: ['public_profile', 'email']}))
+router.get('/facebook/callback',
+    passport.authenticate('facebook', {successRedirect: '/', failureRedirect: '/login'}))
+
+module.exports = router
