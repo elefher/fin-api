f907a28b20ec4aa784017863f9c02ece7bbd4f6a
elefher <eldelielev@gmail.com>
2020-10-14 17:03:57 +0100

    test(accounts): ammendments after code review
    feat #3

--- __tests__/http/controllers/AccountTypesController.test.js
+++ __tests__/http/controllers/AccountTypesController.test.js
@@ -10,162 +10,144 @@ AccountTypeService.fetchAll = jest.fn()
 AccountTypeService.fetchById = jest.fn()
 AccountTypeService.deleteById = jest.fn()
 
-const errorMessage = 'Error message'
-const BoomNotFoundError = Boom.notFound(errorMessage)
-const BoomBadDataError = Boom.badData(errorMessage)
+describe('Testing AccountTypesController', () => {
+  const errorMessage = 'Error message'
+  const BoomNotFoundError = Boom.notFound(errorMessage)
+  const BoomBadDataError = Boom.badData(errorMessage)
 
-let req, res, next
+  let req, res, next
 
-beforeEach(() => {
-  req = httpMocks.createRequest()
-  req.body = {}
-  res = httpMocks.createResponse()
-  next = jest.fn().mockImplementation(err => {
-    return Boom.isBoom(err)
-      ? res.status(err.output.statusCode).json(err.output.payload)
-      : res.status(err.status || 500).json({ message: err.message })
-  })
-})
-
-describe('Create account types', () => {
-  it('should have a create function', () => {
-    expect(typeof AccountTypes.create).toBe('function')
+  beforeEach(() => {
+    req = httpMocks.createRequest()
+    req.body = {}
+    res = httpMocks.createResponse()
+    next = jest.fn().mockImplementation(err => {
+      return Boom.isBoom(err)
+        ? res.status(err.output.statusCode).json(err.output.payload)
+        : res.status(err.status || 500).json({ message: err.message })
+    })
   })
 
-  it('should call AccountTypeService.create', async () => {
-    await AccountTypes.create(req, res, next)
-    expect(AccountTypeService.create).toBeCalledWith(req.body)
+  describe('Create account types', () => {
+    it('should call AccountTypeService.create', async () => {
+      await AccountTypes.create(req, res, next)
+      expect(AccountTypeService.create).toBeCalledWith(req.body)
+    })
+
+    it('should return CREATED response code', async () => {
+      await AccountTypes.create(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.CREATED)
+    })
+
+    it('should return error', async () => {
+      AccountTypeService.create.mockReturnValue(BoomBadDataError)
+
+      await AccountTypes.create(req, res, next)
+
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.UNPROCESSABLE_ENTITY)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 
-  it('should return CREATED response code', async () => {
-    await AccountTypes.create(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.CREATED)
+  describe('Update account types', () => {
+    it('should call AccountTypeService.update', async () => {
+      req.params.id = 1
+      await AccountTypes.update(req, res, next)
+      expect(AccountTypeService.update).toBeCalledWith(req.params.id, req.body)
+    })
+
+    it('should return OK response code', async () => {
+      await AccountTypes.update(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.OK)
+    })
+
+    it('should return error', async () => {
+      AccountTypeService.update.mockReturnValue(BoomNotFoundError)
+
+      await AccountTypes.update(req, res, next)
+
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 
-  it('should return Boom error', async () => {
-    AccountTypeService.create.mockReturnValue(BoomBadDataError)
+  describe('Remove account type', () => {
+    it('should call AccountTypeService.deleteById', async () => {
+      req.params.id = 1
+      await AccountTypes.remove(req, res, next)
+      expect(AccountTypeService.deleteById).toBeCalledWith(req.params.id)
+    })
 
-    await AccountTypes.create(req, res, next)
+    it('should return ACCEPTED response code', async () => {
+      await AccountTypes.remove(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.ACCEPTED)
+    })
 
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomBadDataError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.UNPROCESSABLE_ENTITY)
-    expect(jsonResponse.message).toBe(errorMessage)
-  })
-})
+    it('should return error', async () => {
+      AccountTypeService.deleteById.mockReturnValue(BoomNotFoundError)
 
-describe('Update account types', () => {
-  it('should have an update function', () => {
-    expect(typeof AccountTypes.update).toBe('function')
-  })
+      await AccountTypes.remove(req, res, next)
 
-  it('should call AccountTypeService.update', async () => {
-    req.params.id = 1
-    await AccountTypes.update(req, res, next)
-    expect(AccountTypeService.update).toBeCalledWith(req.params.id, req.body)
-  })
-
-  it('should return OK response code', async () => {
-    await AccountTypes.update(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.OK)
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 
-  it('should return Boom error', async () => {
-    AccountTypeService.update.mockReturnValue(BoomNotFoundError)
+  describe('Fetch all account types', () => {
+    it('should call AccountTypeService.fetchAll', async () => {
+      await AccountTypes.list(req, res, next)
+      expect(AccountTypeService.fetchAll).toBeCalled()
+    })
 
-    await AccountTypes.update(req, res, next)
+    it('should return OK response code', async () => {
+      await AccountTypes.list(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.OK)
+    })
 
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomNotFoundError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
-    expect(jsonResponse.message).toBe(errorMessage)
-  })
-})
+    it('should return error', async () => {
+      AccountTypeService.fetchAll.mockReturnValue(BoomNotFoundError)
 
-describe('Remove account type', () => {
-  it('should have a remove function', () => {
-    expect(typeof AccountTypes.remove).toBe('function')
-  })
+      await AccountTypes.list(req, res, next)
 
-  it('should call AccountTypeService.deleteById', async () => {
-    req.params.id = 1
-    await AccountTypes.remove(req, res, next)
-    expect(AccountTypeService.deleteById).toBeCalledWith(req.params.id)
-  })
-
-  it('should return ACCEPTED response code', async () => {
-    await AccountTypes.remove(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.ACCEPTED)
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 
-  it('should return Boom error', async () => {
-    AccountTypeService.deleteById.mockReturnValue(BoomNotFoundError)
+  describe('View account type', () => {
+    it('should call AccountTypeService.fetchById', async () => {
+      req.params.id = 1
+      await AccountTypes.view(req, res, next)
+      expect(AccountTypeService.fetchById).toBeCalledWith(req.params.id)
+    })
 
-    await AccountTypes.remove(req, res, next)
+    it('should return OK response code', async () => {
+      await AccountTypes.view(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.OK)
+    })
 
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomNotFoundError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
-    expect(jsonResponse.message).toBe(errorMessage)
-  })
-})
+    it('should return error', async () => {
+      AccountTypeService.fetchById.mockReturnValue(BoomNotFoundError)
 
-describe('Fetch all account types', () => {
-  it('should have a list function', () => {
-    expect(typeof AccountTypes.list).toBe('function')
-  })
+      await AccountTypes.view(req, res, next)
 
-  it('should call AccountTypeService.fetchAll', async () => {
-    await AccountTypes.list(req, res, next)
-    expect(AccountTypeService.fetchAll).toBeCalled()
-  })
-
-  it('should return OK response code', async () => {
-    await AccountTypes.list(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.OK)
-  })
-
-  it('should return Boom error', async () => {
-    AccountTypeService.fetchAll.mockReturnValue(BoomNotFoundError)
-
-    await AccountTypes.list(req, res, next)
-
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomNotFoundError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
-    expect(jsonResponse.message).toBe(errorMessage)
-  })
-})
-
-describe('View account type', () => {
-  it('should have a view function', () => {
-    expect(typeof AccountTypes.view).toBe('function')
-  })
-
-  it('should call AccountTypeService.fetchById', async () => {
-    req.params.id = 1
-    await AccountTypes.view(req, res, next)
-    expect(AccountTypeService.fetchById).toBeCalledWith(req.params.id)
-  })
-
-  it('should return OK response code', async () => {
-    await AccountTypes.view(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.OK)
-  })
-
-  it('should return Boom error', async () => {
-    AccountTypeService.fetchById.mockReturnValue(BoomNotFoundError)
-
-    await AccountTypes.view(req, res, next)
-
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomNotFoundError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
-    expect(jsonResponse.message).toBe(errorMessage)
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 })
--- __tests__/http/controllers/AccountsController.test.js
+++ __tests__/http/controllers/AccountsController.test.js
@@ -10,169 +10,166 @@ AccountsService.fetchAllByUser = jest.fn()
 AccountsService.fetchByIdAndUser = jest.fn()
 AccountsService.deleteByIdAndUser = jest.fn()
 
-const errorMessage = 'Error message'
-const BoomNotFoundError = Boom.notFound(errorMessage)
-const BoomBadDataError = Boom.badData(errorMessage)
+describe('Testing AccountsController', () => {
+  const errorMessage = 'Error message'
+  const BoomNotFoundError = Boom.notFound(errorMessage)
+  const BoomBadDataError = Boom.badData(errorMessage)
+  let req, res, next
 
-let req, res, next
-
-beforeEach(() => {
-  req = httpMocks.createRequest()
-  req.body = { user: 'userId123' }
-  res = httpMocks.createResponse()
-  next = jest.fn().mockImplementation(err => {
-    return Boom.isBoom(err)
-      ? res.status(err.output.statusCode).json(err.output.payload)
-      : res.status(err.status || 500).json({ message: err.message })
-  })
-})
-
-describe('Create account', () => {
-  it('should have a create function', () => {
-    expect(typeof Accounts.create).toBe('function')
+  beforeEach(() => {
+    req = httpMocks.createRequest()
+    req.body = { user: 'userId123' }
+    res = httpMocks.createResponse()
+    next = jest.fn().mockImplementation(err => {
+      return Boom.isBoom(err)
+        ? res.status(err.output.statusCode).json(err.output.payload)
+        : res.status(err.status || 500).json({ message: err.message })
+    })
   })
 
-  it('should call AccountsService.create', async () => {
-    await Accounts.create(req, res, next)
-    expect(AccountsService.create).toBeCalledWith(req.body)
+  describe('Create account', () => {
+    it('should call AccountsService.create', async () => {
+      await Accounts.create(req, res, next)
+      expect(AccountsService.create).toBeCalledWith(req.body)
+    })
+
+    it('should return CREATED response code', async () => {
+      await Accounts.create(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.CREATED)
+    })
+
+    it('should return error', async () => {
+      AccountsService.create.mockReturnValue(BoomBadDataError)
+
+      await Accounts.create(req, res, next)
+
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.UNPROCESSABLE_ENTITY)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 
-  it('should return CREATED response code', async () => {
-    await Accounts.create(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.CREATED)
+  describe('Update account types', () => {
+    it('should have an update function', () => {
+      expect(typeof Accounts.update).toBe('function')
+    })
+
+    it('should call AccountService.update', async () => {
+      req.params.id = 1
+      await Accounts.update(req, res, next)
+      expect(AccountsService.update).toBeCalledWith(req.params.id, req.body)
+    })
+
+    it('should return OK response code', async () => {
+      await Accounts.update(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.OK)
+    })
+
+    it('should return error', async () => {
+      AccountsService.update.mockReturnValue(BoomNotFoundError)
+
+      await Accounts.update(req, res, next)
+
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 
-  it('should return Boom error', async () => {
-    AccountsService.create.mockReturnValue(BoomBadDataError)
+  describe('Remove account', () => {
+    it('should have a remove function', () => {
+      expect(typeof Accounts.remove).toBe('function')
+    })
 
-    await Accounts.create(req, res, next)
+    it('should call AccountsService.deleteByIdAndUser', async () => {
+      req.params.id = 1
+      await Accounts.remove(req, res, next)
+      expect(AccountsService.deleteByIdAndUser).toBeCalledWith(
+        req.params.id,
+        req.body.user,
+      )
+    })
 
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomBadDataError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.UNPROCESSABLE_ENTITY)
-    expect(jsonResponse.message).toBe(errorMessage)
-  })
-})
+    it('should return ACCEPTED response code', async () => {
+      await Accounts.remove(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.ACCEPTED)
+    })
 
-describe('Update account types', () => {
-  it('should have an update function', () => {
-    expect(typeof Accounts.update).toBe('function')
-  })
+    it('should return Boom error', async () => {
+      AccountsService.deleteByIdAndUser.mockReturnValue(BoomNotFoundError)
 
-  it('should call AccountService.update', async () => {
-    req.params.id = 1
-    await Accounts.update(req, res, next)
-    expect(AccountsService.update).toBeCalledWith(req.params.id, req.body)
-  })
+      await Accounts.remove(req, res, next)
 
-  it('should return OK response code', async () => {
-    await Accounts.update(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.OK)
-  })
-
-  it('should return Boom error', async () => {
-    AccountsService.update.mockReturnValue(BoomNotFoundError)
-
-    await Accounts.update(req, res, next)
-
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomNotFoundError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
-    expect(jsonResponse.message).toBe(errorMessage)
-  })
-})
-
-describe('Remove account', () => {
-  it('should have a remove function', () => {
-    expect(typeof Accounts.remove).toBe('function')
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalledWith(BoomNotFoundError)
+      expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 
-  it('should call AccountsService.deleteByIdAndUser', async () => {
-    req.params.id = 1
-    await Accounts.remove(req, res, next)
-    expect(AccountsService.deleteByIdAndUser).toBeCalledWith(
-      req.params.id,
-      req.body.user,
-    )
+  describe('Fetch all account types', () => {
+    it('should have a list function', () => {
+      expect(typeof Accounts.list).toBe('function')
+    })
+
+    it('should call AccountService.fetchAllByUser', async () => {
+      await Accounts.list(req, res, next)
+      expect(AccountsService.fetchAllByUser).toBeCalled()
+    })
+
+    it('should return OK response code', async () => {
+      await Accounts.list(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.OK)
+    })
+
+    it('should return error', async () => {
+      AccountsService.fetchAllByUser.mockReturnValue(BoomNotFoundError)
+
+      await Accounts.list(req, res, next)
+
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 
-  it('should return ACCEPTED response code', async () => {
-    await Accounts.remove(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.ACCEPTED)
-  })
+  describe('View account type', () => {
+    it('should have a view function', () => {
+      expect(typeof Accounts.view).toBe('function')
+    })
 
-  it('should return Boom error', async () => {
-    AccountsService.deleteByIdAndUser.mockReturnValue(BoomNotFoundError)
+    it('should call AccountService.fetchByIdAndUser', async () => {
+      req.params.id = 1
+      req.body.user = 'userid123'
+      await Accounts.view(req, res, next)
+      expect(AccountsService.fetchByIdAndUser).toBeCalledWith(
+        req.params.id,
+        req.body.user,
+      )
+    })
 
-    await Accounts.remove(req, res, next)
+    it('should return OK response code', async () => {
+      await Accounts.view(req, res, next)
+      expect(res._isEndCalled()).toBeTruthy()
+      expect(res.statusCode).toBe(StatusCodes.OK)
+    })
 
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomNotFoundError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
-    expect(jsonResponse.message).toBe(errorMessage)
-  })
-})
+    it('should return error', async () => {
+      AccountsService.fetchByIdAndUser.mockReturnValue(BoomNotFoundError)
 
-describe('Fetch all account types', () => {
-  it('should have a list function', () => {
-    expect(typeof Accounts.list).toBe('function')
-  })
+      await Accounts.view(req, res, next)
 
-  it('should call AccountService.fetchAllByUser', async () => {
-    await Accounts.list(req, res, next)
-    expect(AccountsService.fetchAllByUser).toBeCalled()
-  })
-
-  it('should return OK response code', async () => {
-    await Accounts.list(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.OK)
-  })
-
-  it('should return Boom error', async () => {
-    AccountsService.fetchAllByUser.mockReturnValue(BoomNotFoundError)
-
-    await Accounts.list(req, res, next)
-
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomNotFoundError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
-    expect(jsonResponse.message).toBe(errorMessage)
-  })
-})
-
-describe('View account type', () => {
-  it('should have a view function', () => {
-    expect(typeof Accounts.view).toBe('function')
-  })
-
-  it('should call AccountService.fetchByIdAndUser', async () => {
-    req.params.id = 1
-    req.body.user = 'userid123'
-    await Accounts.view(req, res, next)
-    expect(AccountsService.fetchByIdAndUser).toBeCalledWith(
-      req.params.id,
-      req.body.user,
-    )
-  })
-
-  it('should return OK response code', async () => {
-    await Accounts.view(req, res, next)
-    expect(res._isEndCalled()).toBeTruthy()
-    expect(res.statusCode).toBe(StatusCodes.OK)
-  })
-
-  it('should return Boom error', async () => {
-    AccountsService.fetchByIdAndUser.mockReturnValue(BoomNotFoundError)
-
-    await Accounts.view(req, res, next)
-
-    const jsonResponse = JSON.parse(res._getData())
-    expect(next).toBeCalledWith(BoomNotFoundError)
-    expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
-    expect(jsonResponse.message).toBe(errorMessage)
+      const jsonResponse = JSON.parse(res._getData())
+      expect(next).toBeCalled()
+      expect(jsonResponse.statusCode).toBe(StatusCodes.NOT_FOUND)
+      expect(jsonResponse.message).toBe(errorMessage)
+    })
   })
 })
